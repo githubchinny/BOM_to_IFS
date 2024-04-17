54b464bde63e2d12f4e7ac683faabbbd332c0a0a
mark <anovadatasolutions@gmail.com>
2024-04-17 08:46:41 +0100

    add excel_formatting to BOM_to_IFS folder

---
+++ excel_formatting.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+def adjust_col_width_from_col(ws):
+
+    dims = {}
+    for row in ws.rows:
+        for cell in row:
+            if cell.value:
+                dims[cell.column_letter] = max((dims.get(cell.column_letter, 0), len(str(cell.value))))
+    for col, value in dims.items():
+        ws.column_dimensions[col].width = value * 1.25
+
+
